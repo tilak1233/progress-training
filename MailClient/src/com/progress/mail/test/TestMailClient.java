282e19faf3731ad56de1ff1f3174bbfae0d6cf39
chethan2k5 <chethan2k5@5e8100ea-efa1-11de-aed4-3956c0e5a634>
2009-12-29 10:06:13 +0000

    added test class

---
+++ MailClient/src/com/progress/mail/test/TestMailClient.java
@@ -0,0 +1,13 @@
+package com.progress.mail.test;
+
+import com.progress.mail.client.msg.Client;
+
+public class TestMailClient {
+
+	public static void main(String[] args) {
+
+		Client clientObj = new Client();
+		clientObj.startClient();
+	}
+
+}
